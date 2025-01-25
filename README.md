2cf149db181d4cb2c51c2c3e56e8f1038f45c5ba
David Maignan <davidmaignan@gmail.com>
2025-01-25 10:29:24 -0500

    Poll result completed

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ To start the Phoenix server:
   * Start Phoenix endpoint with `mix phx.server` or inside IEx with `iex -S mix phx.server`
 
 Now you can visit [`localhost:4000`](http://localhost:4000) from your browser.
+
+
+```
+self()
+:observer.start()
+```
--- lib/tutorial/poll_results.ex
+++ lib/tutorial/poll_results.ex
@@ -1,5 +1,8 @@
 defmodule Tutorial.PollResults do
+  alias Phoenix.PubSub
   use GenServer
+
+  @pubsub_topic "poll"
 
   @initial_state %{
     "A" => 0,
@@ -8,12 +11,24 @@ defmodule Tutorial.PollResults do
     "D" => 0
   }
 
-  def start do
+  def start_link(_) do
     GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
   def get_state do
     GenServer.call(__MODULE__, :get_state)
+  end
+
+  def subscribe_to_updates do
+    PubSub.subscribe(Tutorial.PubSub, @pubsub_topic)
+  end`-`
+
+  def vote(option) do
+    GenServer.cast(__MODULE__, {:vote, option})
+  end
+
+  def reset do
+    GenServer.cast(__MODULE__, :reset)
   end
 
   def init(_) do
@@ -22,5 +37,20 @@ defmodule Tutorial.PollResults do
 
   def handle_call(:get_state, _from, state) do
     {:reply, state, state}
+  end
+
+  def handle_cast({:vote, option}, state) do
+    new_state = Map.update!(state, option, &(&1 + 1))
+    broadcast_update(new_state)
+    {:noreply, new_state}
+  end
+
+  def handle_cast(:reset, _state) do
+    broadcast_update(@initial_state)
+    {:noreply, @initial_state}
+  end
+
+  defp broadcast_update(state) do
+    PubSub.broadcast(Tutorial.PubSub, @pubsub_topic, {:update_results, state})
   end
 end
